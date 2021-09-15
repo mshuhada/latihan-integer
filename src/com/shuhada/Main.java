7e19e1cb6f1c74a6756963f4ad636836929335b6
mshuhada <aatshuada@gmail.com>
2021-09-15 19:35:34 +0700

    tugas 1

---
+++ src/com/shuhada/Main.java
@@ -0,0 +1,17 @@
+package com.shuhada;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nama = "budi";
+
+        System.out.println(nama);
+
+        nama = "tarmanto";
+        System.out.println(nama);
+
+        nama = "santi";
+        System.out.println(nama);
+    }
+}
